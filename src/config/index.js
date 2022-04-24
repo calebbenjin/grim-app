775e41f4c239395da358ba185ffb25e1a177a91a
calebbenjin <calebben94@gmail.com>
2022-04-24 22:22:50 +0100

    NEXT_PUBLIC_API_URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://wdremit.herokuapp.com/api/v1';
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://wdremit.herokuapp.com/api/v1';
 
 export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
